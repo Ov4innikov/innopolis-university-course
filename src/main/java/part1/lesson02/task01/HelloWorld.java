43d64a388acc67c704d346b7f3d6f2b9fa4107f3
CrazyEugene <WolF2010502@users.noreply.github.com>
2019-04-18 09:39:51 +0300

    Edit HelloWorld class

--- src/main/java/part1/lesson02/task01/HelloWorld.java
+++ src/main/java/part1/lesson02/task01/HelloWorld.java
@@ -1,4 +1,14 @@
 package part1.lesson02.task01;
 
+/**
+ * Класс решения первого задания.
+ * @autor Овчинников Евгений
+ * @version 1.0.0
+ */
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        throw new HelloWorldException();
+    }
 }
